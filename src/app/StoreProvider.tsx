f793c31e193b01b34f60ba32f46f611741d9a36d
gracebir <grace.devolop@gmail.com>
2024-10-12 17:53:23 +0200

    authention

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -3,11 +3,18 @@
 "use client";
 import { store } from "@/lib/store";
 import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function StoreProvider({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    return <Provider store={store}>{children}</Provider>;
+    return (
+        <Provider store={store}>
+            {children}
+            <ToastContainer />
+        </Provider>
+    );
 }
