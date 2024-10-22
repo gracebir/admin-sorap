7b10ba03ee0f174491c7696248aa662dcce6f253
gracebir <grace.devolop@gmail.com>
2024-10-22 17:06:03 +0200

    install lucide react

--- src/helper/funct.ts
+++ src/helper/funct.ts
@@ -1,5 +1,7 @@
 /** @format */
 
+import { TCreateEventInput } from "@/types/event";
+
 export function formatNumber(num: number) {
     if (num >= 1000) {
         return (num / 1000).toFixed(num % 1000 === 0 ? 0 : 1) + "k";
@@ -10,3 +12,19 @@ export function formatNumber(num: number) {
 export function formatRate(rate: number) {
     return (rate * 100).toFixed(0) + "%";
 }
+
+export const createEventFormData = (input: TCreateEventInput): FormData => {
+    const formData = new FormData();
+
+    // Append each field from TCreateEventInput to the FormData
+    formData.append("theme", input.theme);
+    formData.append("description", input.description);
+    formData.append("start_date", input.start_date.toISOString()); // Convert Date to string
+    formData.append("end_date", input.end_date.toISOString()); // Convert Date to string
+    formData.append("location", input.location);
+    formData.append("price", input.price.toString()); // Convert number to string
+    formData.append("thumbnail", input.thumbnail!); // Blob (File)
+    formData.append("eventType", input.eventType);
+
+    return formData;
+};
