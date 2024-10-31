aa9c3f8a3bf6748516fb0b01fa17b2bbd0a484d4
gracebir <grace.devolop@gmail.com>
2024-10-31 19:05:31 +0200

    edit program

--- src/app/admin/program/edit/[id]/page.tsx
+++ src/app/admin/program/edit/[id]/page.tsx
@@ -1,7 +1,14 @@
 /** @format */
 
+import FormEditProgram from "@/components/program/edit/FormEditProgram";
+import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import React from "react";
 
-export default function EditProgram() {
-    return <div></div>;
+export default function EditProgram({ params }: { params: Params }) {
+    const { id } = params;
+    return (
+        <main>
+            <FormEditProgram id={parseInt(id)} />
+        </main>
+    );
 }
