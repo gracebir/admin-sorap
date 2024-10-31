f91f86197ceba8ef686cac8413b15ecc15dacc98
gracebir <grace.devolop@gmail.com>
2024-10-31 18:20:05 +0200

    edit and update program pages

---
+++ src/app/admin/program/create-program/page.tsx
@@ -0,0 +1,12 @@
+/** @format */
+
+import FormProgram from "@/components/program/create-program/FormProgram";
+import React from "react";
+
+export default function CreatePage() {
+    return (
+        <main>
+            <FormProgram />
+        </main>
+    );
+}
