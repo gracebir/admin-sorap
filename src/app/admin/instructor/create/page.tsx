c6e83cdb1078460e1f93a54bc4542b692b9c87b6
gracebir <grace.devolop@gmail.com>
2024-11-02 20:39:55 +0200

    instructor page

---
+++ src/app/admin/instructor/create/page.tsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import FormInstructor from "@/components/instructor/create-instructor/FormInstructor";
+import React from "react";
+
+function CreateInstructor() {
+    return (
+        <main>
+            <FormInstructor />
+        </main>
+    );
+}
+
+export default CreateInstructor;
