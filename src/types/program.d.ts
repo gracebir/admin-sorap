52127298a8d84e6f3ba2ace50c8cce48e7d58967
gracebir <grace.devolop@gmail.com>
2024-12-28 19:01:32 +0200

    type and schema

--- src/types/program.d.ts
+++ src/types/program.d.ts
@@ -1,68 +1,82 @@
 /** @format */
 
 export type TcreateProgramEntry = {
-    title: string;
-    description: string;
-    edition: string;
-    programCategory: string;
-    location: string;
-    thumbnail: File | null;
-    price?: number;
-    date_from: string;
-    date_to: string;
+  title: string;
+  description: string;
+  edition: string;
+  programCategory: string;
+  location: string;
+  thumbnail: File | null;
+  price?: number;
+  date_from: string;
+  date_to: string;
 };
 
 export type TprogramType = {
-    id?: number;
-    title: string;
-    description: string;
-    edition: string; // Unique constraint
-    images?: string[]; // Optional, defaults to an empty array
-    thumbnail: string;
-    price?: number; // Optional, defaults to 0
-    date_from: string;
-    isLaunched: boolean;
-    date_to: string;
+  id?: number;
+  title: string;
+  description: string;
+  edition: string; // Unique constraint
+  images?: string[]; // Optional, defaults to an empty array
+  thumbnail: string;
+  price?: number; // Optional, defaults to 0
+  date_from: string;
+  isLaunched: boolean;
+  date_to: string;
 };
 
 export type TprogramState = {
-    programs: Array<TprogramType> | null;
+  programs: Array<TprogramType> | null;
 };
 
 export type TprogramReponse = {
-    status: string;
-    message: string;
-    data: Array<TprogramType>;
+  status: string;
+  message: string;
+  data: Array<TprogramType>;
 };
 
 export type TsponsorInput = {
-    sponsorId: number;
-    userId: number;
+  sponsorId: number;
+  userId: number;
+};
+
+export type TCreateProgramTranslation = {
+  programId: number;
+  title: string;
+  description: string;
+  language: string;
+};
+
+export type TProgramTranslation = {
+  id: number;
+  programId: number;
+  title: string;
+  description: string;
 };
 
 export type TsponsorState = {
-    sponsor: any;
+  sponsor: any;
 };
 
 export type TtutorInput = {
-    programId: number;
-    instructorId: number;
+  programId: number;
+  instructorId: number;
 };
 
 export type TtutorResponse = {
-    status: string;
-    message: string;
-    data: Array<{
-        programId: number;
-        instructor: {
-            firstname: string;
-            lastname: string;
-        };
-        program: {
-            title: string;
-            description: string;
-            id: number;
-            thumbnail: string;
-        };
-    }>;
+  status: string;
+  message: string;
+  data: Array<{
+    programId: number;
+    instructor: {
+      firstname: string;
+      lastname: string;
+    };
+    program: {
+      title: string;
+      description: string;
+      id: number;
+      thumbnail: string;
+    };
+  }>;
 };
