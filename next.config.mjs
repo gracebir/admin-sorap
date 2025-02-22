d79bdac8862bc59e4a1f5a403183f9a8411bf3d6
gracebir <grace.devolop@gmail.com>
2025-02-22 20:38:42 +0200

    build project

--- next.config.mjs
+++ next.config.mjs
@@ -4,17 +4,17 @@
  */
 
 const nextConfig = {
-    transpilePackages: ["@mdxeditor/editor"],
-    images: {
-        remotePatterns: [
-            {
-                hostname: "res.cloudinary.com",
-            },
-            {
-                hostname: "miro.medium.com",
-            },
-        ],
-    },
+  transpilePackages: ["@mdxeditor/editor"],
+  images: {
+    remotePatterns: [
+      {
+        hostname: "res.cloudinary.com",
+      },
+      {
+        hostname: "miro.medium.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
